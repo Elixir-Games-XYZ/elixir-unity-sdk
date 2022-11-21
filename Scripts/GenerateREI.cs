adeffd3ddabc83ca214d9b631ba58e50b6461eec
Jorge Gonzalez Regalado <61213914+jregaladoo@users.noreply.github.com>
2022-11-21 11:51:08 +0100

    Fix: Update predefined sandbox JWT

--- Scripts/GenerateREI.cs
+++ Scripts/GenerateREI.cs
@@ -11,7 +11,7 @@ namespace Elixir {
         static GenerateREIResponse response = new GenerateREIResponse();
         
         public static IEnumerator Do() {
-            Auth.token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI5YmM4MTgzOC00NTY0LTQ4M2UtYmE2MC1iY2JiZjE2YmM1MjQiLCJlbWFpbCI6ImZlcm5hbmRvdGVzdDFAc2F0b3NoaXMuZ2FtZXMiLCJpYXQiOjE2Mjc0ODUxODYsImV4cCI6MjE1NTQ4NTE4Nn0.SAkEr9-CWwnLSNup7X1d9E3vmSj3mlPQA_5Z2p54m__hPsL765xFxvUvjRpKwMhxrAE9QirAodP7868_yA0s9A";
+            Auth.token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI5YmM4MTgzOC00NTY0LTQ4M2UtYmE2MC1iY2JiZjE2YmM1MjQiLCJlbWFpbCI6ImZlcm5hbmRvdGVzdDFAc2F0b3NoaXMuZ2FtZXMiLCJlbnZpcm9ubWVudCI6ImVsaXhpciIsImlhdCI6MTY2ODg1OTI0MiwiZXhwIjoxNzI4ODU5MjQyfQ._S9OJMUaUzkKZRJdiVjeGSzHzqFZhMWIUY3BmZla5etMecSRGFDH6BwRj3zEGygof9pVVZlkrYMiHGpKGcZUxQ";
             yield return Get($"/dev/reikey/{GameID}", response);
             ElixirController.Instance.rei = response.reikey;
         }
